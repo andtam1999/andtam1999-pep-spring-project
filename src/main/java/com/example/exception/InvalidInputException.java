3233832975352681a15ea827b8fc73109fa4958e
Andric Tam <andtam1999@gmail.com>
2024-11-12 19:50:16 +0000

    Removed unused parameter in exception class

--- src/main/java/com/example/exception/InvalidInputException.java
+++ src/main/java/com/example/exception/InvalidInputException.java
@@ -2,8 +2,6 @@ package com.example.exception;
 
 public class InvalidInputException extends Exception {
 
-    String invalidParam;
-
     public InvalidInputException(String errorMsg) {
         super(errorMsg);
     }
