df58bbeff8e40f0b669085b43ed837fab25b490a
andtam1999 <andtam1999@gmail.com>
2025-01-03 14:46:43 -0500

    Update MessageRepository.java
    Updated comment

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -5,6 +5,6 @@ import com.example.entity.Message;
 import org.springframework.data.jpa.repository.*;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-    public Message getByMessageId(int messageId); // not sure why but default geById does not work
+    public Message getByMessageId(int messageId); // default GetById is deprecated
     public List<Message> findAllByPostedBy(int postedBy);
 }
